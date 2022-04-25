b9f867a8e94d88d2f1ceb197267aa317a5dd6a30
Denis Lazykin <dlazykin856@gmail.com>
2022-04-25 11:42:39 +0300

    4.1. Разбор первой программы. [#159033 #228653]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.printf("1 * 1 = 1");
+        System.out.printf("1 * 2 = 2");
+        System.out.printf("1 * 3 = 3");
+        System.out.printf("1 * 4 = 4");
+        System.out.printf("1 * 5 = 5");
+        System.out.printf("1 * 6 = 6");
+        System.out.printf("1 * 7 = 7");
+        System.out.printf("1 * 8 = 8");
+        System.out.printf("1 * 9 = 9");
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.printf("Denis Lazykin");
+        System.out.printf("11.08.1989");
+    }
+}
