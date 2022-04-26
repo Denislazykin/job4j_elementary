4b8dc42aa14d939118657390af02562e5277a371
Denis Lazykin <dlazykin856@gmail.com>
2022-04-26 11:22:24 +0300

    Программа демонстрирует возможность присоединения разных типов данных

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+/*
+ѕрограмма демонстрирует возможность присоединени€ разных типов данных данных
+ */
+public class Out {
+    public static void main(String[] args) {
+        String name = "Petr";
+        byte age = 33;
+        String separate = " - ";
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+    }
+}
