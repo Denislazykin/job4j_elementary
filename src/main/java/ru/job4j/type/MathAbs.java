6b1d0d5108b49366c988daae1abe9a21f0fc8026
Denis Lazykin <dlazykin856@gmail.com>
2022-04-28 10:04:54 +0300

    8. Модуль числа

---
+++ src/main/java/ru/job4j/type/MathAbs.java
@@ -0,0 +1,10 @@
+package ru.job4j.type;
+/*
+Получаем модуль числа
+ */
+public class MathAbs {
+    public static void main(String[] args) {
+        double result = Math.abs(-5);
+        System.out.println(result);
+    }
+}
