b5da0f637cb348d179ac8b426bd51d87da38e3e6
Denis Lazykin <dlazykin856@gmail.com>
2022-04-28 09:46:41 +0300

    7. Получение корня из числа

---
+++ src/test/java/ru/job4j/type/MathSqrt.java
@@ -0,0 +1,10 @@
+package ru.job4j.type;
+/*
+Получаем корень из числа
+ */
+public class MathSqrt {
+    public static void main(String[] args) {
+        double result = Math.sqrt(9);
+        System.out.println(result);
+    }
+}
