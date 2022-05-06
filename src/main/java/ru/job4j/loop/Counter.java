28f70ecd14803473d3930db04648af65faba643b
Denis Lazykin <dlazykin856@gmail.com>
2022-05-06 15:35:28 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192 #233094]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -1,9 +1,9 @@
 package ru.job4j.loop;
 
-/*
-Программа, которая подсчитывает сумму чисел от start до finish
- */
 public class Counter {
+    /*
+    Считает сумму чисел от start до finish
+    */
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
@@ -12,9 +12,34 @@ public class Counter {
         return sum;
     }
 
+    /*
+    Считает сумму четных чисел от start до finish
+     */
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+    /*
+    Вычисляем остаток от деления двух чисел
+     */
+    public static int modulo(int number, int divider) {
+        while (number >= divider) {
+            number -= divider;
+        }
+        return number;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(0, 5));
+        System.out.println(sumByEven(1, 10));
+        System.out.println(modulo(21, 16));
     }
 }
