a42e5dd872bf61471487a9b6880d5677d4eaff38
Denis Lazykin <dlazykin856@gmail.com>
2022-05-07 14:28:25 +0300

    5.4. Протеиновая диета [#156313 #235853]

---
+++ src/main/java/ru/job4j/loop/Income.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+/*
+Демонстрация работы цикла while
+ */
+public class Income {
+    public static void main(String[] args) {
+        int income = 0;
+        while (income < 100) {
+            System.out.println(income);
+            income += 10;
+        }
+    }
+}
