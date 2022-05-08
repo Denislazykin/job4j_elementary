6d67e85548247097b307036046258c0103d1dd8d
Denis Lazykin <dlazykin856@gmail.com>
2022-05-08 08:09:01 +0300

    edit Mortgage

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,11 +1,14 @@
 package ru.job4j.loop;
 
+/*
+Считаем количество лет для погашения кредита
+ */
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amount >= salary) {
-            double sum = amount * percent;
+        while (amount > 0) {
             year++;
+            amount = amount + amount * percent / 100 - salary;
         }
         return year;
     }
