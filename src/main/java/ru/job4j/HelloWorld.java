f443e35c9834f04f668d971d0adb0127502515b7
Денис <dzaka89@gmail.com>
2022-06-15 18:38:22 +0300

    test

--- src/main/java/ru/job4j/HelloWorld.java
+++ src/main/java/ru/job4j/HelloWorld.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Job4j!");
+        System.out.println("Hello, Job4j! Let's started study");
     }
 }
